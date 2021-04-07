a6306688a9e39e6a419323f36eb4c4c060b7b9df
Paul <55263579+pburslemjr@users.noreply.github.com>
2021-04-07 15:51:27 -0500

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+
+
+<?php
+phpinfo();
+    echo 'Connection test : ';
+    
+    $connection = pg_connect("host=csce-315-db.engr.tamu.edu port=5432 dbname=db_group4_project3 user=paul_b_tamu password=227007259 connect_timeout=5");
+    if($connection) {
+       echo 'connected';
+    } else {
+        echo 'there has been an error connecting';
+    } 
+?>
---
+++ login.php
@@ -0,0 +1,55 @@
+<?php 
+
+include('config.php');
+echo("Testing");
+
+
+session_start(); /* Starts the session */
+
+/* Check Login form submitted*//* if(isset($_POST['Submit'])){
+$Username = isset($_POST['Username']) ? $_POST['Username'] : '';
+$Password = isset($_POST['Password']) ? $_POST['Password'] : '';
+
+$query = $db_connection->prepare("SELECT * FROM users WHERE username=:Username");
+$query->bindParam("Username", $Username, PDO::PARAM_STR);
+$query->execute();
+
+$result = $query->fetch(PDO::FETCH_ASSOC);
+
+if (!$result) {
+	$msg="<span style='color:red'>Invalid Login Details</span>";
+} else {
+if (password_verify($Password, $result['password'])) {
+$_SESSION['UserData']['Username']=$logins[$Username];
+header("location:index.php");
+exit;
+} else {
+$msg="<span style='color:red'>Invalid Login Details</span>";
+}
+}
+*/
+?>
+<form action="" method="post" name="Login_Form">
+  <table width="400" border="0" align="center" cellpadding="5" cellspacing="1" class="Table">
+    <?php if(isset($msg)){?>
+    <tr>
+      <td colspan="2" align="center" valign="top"><?php echo $msg;?></td>
+    </tr>
+    <?php } ?>
+    <tr>
+      <td colspan="2" align="left" valign="top"><h3>Login</h3></td>
+    </tr>
+    <tr>
+      <td align="right" valign="top">Username</td>
+      <td><input name="Username" type="text" class="Input"></td>
+    </tr>
+    <tr>
+      <td align="right">Password</td>
+      <td><input name="Password" type="password" class="Input"></td>
+    </tr>
+    <tr>
+      <td> </td>
+      <td><input name="Submit" type="submit" value="Login" class="Button3"></td>
+    </tr>
+  </table>
+</form>
