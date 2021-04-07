81e468c6236e14f971cba339724bb986aeadc266
Paul Burslem <pburslemjr@gmail.com>
2021-04-07 16:45:45 -0500

    connection modifed

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
 
     echo 'Connection test : ';
     
-    $connection = pg_connect("host=csce-315-db.engr.tamu.edu dbname=db906_group4_project3 user=paul_b_tamu password=227007259 connect_timeout=5");
+    $connection = pg_connect("host=csce-315-db.engr.tamu.edu port=5432 dbname=db906_group4_project3 user=paul_b_tamu password=227007259 connect_timeout=5");
     if($connection) {
        echo 'connected';
     } else {
