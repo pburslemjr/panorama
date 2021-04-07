707c90fcacb4ca02450dc3a3e0e3e11bf6742b63
Paul Burslem <pburslemjr@gmail.com>
2021-04-07 16:21:29 -0500

    removed info command

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 
 
 <?php
-phpinfo();
+
     echo 'Connection test : ';
     
-    $connection = pg_connect("host=csce-315-db.engr.tamu.edu port=5432 dbname=db_group4_project3 user=paul_b_tamu password=227007259 connect_timeout=5");
+    $connection = pg_connect("host=csce-315-db.engr.tamu.edu dbname=db_group4_project3 user=paul_b_tamu password=227007259 connect_timeout=5");
     if($connection) {
        echo 'connected';
     } else {
