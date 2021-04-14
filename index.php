b89d775064b605b3e806a9c928d8f20989f65206
Paul Burslem <pburslemjr@gmail.com>
2021-04-14 15:25:15 -0500

    Login Bugfix

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php session_start(); /* Starts the session */
-if(!isset($_SESSION['UserData']['Username'])){
-echo("Oops no user data");
+if(!isset($_SESSION['users']['username'])){
+
 header("location:login.php");
 exit;
 }
